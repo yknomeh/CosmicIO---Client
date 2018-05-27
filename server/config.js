9589a288731145d979cfde4ea03537922e7cf835
Mikolaj Radkowski <macbooc48@gmail.com>
2018-05-27 12:53:28 +0200

    dobra skiny dzialaja, tyle (chyba) na dzisiaj bo mam gosci ;_;

--- server/config.js
+++ server/config.js
@@ -4,7 +4,7 @@ global.RENDER_SIZE = 5; /* size of dust rendering */
 global.AMOUNT_OF_DUST = 500; /* amount of dust */
 
 global.LOBBY_TIME = 9; /* lobby/hub time */
-global.GAME_TIME = 18; /* game time */
+global.GAME_TIME = 1800; /* game time */
 
 global.STARTING_HP = 100;
 global.SPEED = 5;
